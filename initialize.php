807d995ccaf37a0d510c095411765524b306d822
harvey <agrabioharvey@gmail.com>
2023-12-11 01:24:41 +0800

    db

--- initialize.php
+++ initialize.php
@@ -3,7 +3,7 @@ if(!defined('base_url')) define('base_url','http://localhost/cscs/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"cscs_db");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"u445806455_arguspay");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"FroyPOS11");
+if(!defined('DB_NAME')) define('DB_NAME',"u445806455_arguspay");
 ?>
